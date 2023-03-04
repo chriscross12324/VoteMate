9cd3f62aad7b0029add12d6a9cbc404cf76cbb02
Toma <tomaaitken5@gmail.com>
2023-03-04 15:50:04 -0800

    Added PageRegister.java

--- app/src/main/java/com/ctl/votemate/MainActivity.java
+++ app/src/main/java/com/ctl/votemate/MainActivity.java
@@ -9,15 +9,22 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 private LinearLayout buttonLoginPage;
+private LinearLayout buttonRegisterPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonLoginPage = findViewById(R.id.buttonLogin);
+        buttonRegisterPage = findViewById(R.id.buttonRegister);
+
         buttonLoginPage.setOnClickListener(v -> {
             Intent pageLogin = new Intent(this,PageLogin.class);
             startActivity(pageLogin);
+        });
+        buttonRegisterPage.setOnClickListener(v ->{
+            Intent pageRegister = new Intent(this, PageRegister.class);
+            startActivity(pageRegister);
         });
     }
 }
