98c5a7e1dae155194ee72c255a9c7eb3f92f7d7f
Luka Aitken <T00663672@mytru.ca>
2023-03-04 15:40:15 -0800

    Added Page - PageLogin.java

--- app/src/main/java/com/ctl/votemate/MainActivity.java
+++ app/src/main/java/com/ctl/votemate/MainActivity.java
@@ -2,13 +2,22 @@ package com.ctl.votemate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+private LinearLayout buttonLoginPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonLoginPage = findViewById(R.id.buttonLogin);
+        buttonLoginPage.setOnClickListener(v -> {
+            Intent pageLogin = new Intent(this,PageLogin.class);
+            startActivity(pageLogin);
+        });
     }
 }
