e99696d6abe0aba853b08d9a5b415fbeb4ee071c
Chris <chrismc16@outlook.com>
2023-03-09 15:09:20 -0800

    - Improved the UI of activity_page_login.xml & activity_page_register.xml - Added "Back Button" functionality to PageLogin.java & PageRegister.java

--- app/src/main/java/com/ctl/votemate/PageLogin.java
+++ app/src/main/java/com/ctl/votemate/PageLogin.java
@@ -1,6 +1,7 @@
 package com.ctl.votemate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
@@ -10,5 +11,10 @@ public class PageLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_login);
+
+        CardView buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(n -> {
+            finish();
+        });
     }
 }
