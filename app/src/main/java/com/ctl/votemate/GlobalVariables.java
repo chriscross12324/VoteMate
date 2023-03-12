fb0093807ce932c5fcc93223b86faf0b0d445d90
Chris <chrismc16@outlook.com>
2023-03-11 21:06:51 -0800

    - Added google-services.json; Used to configure Firebase - Added GlobalVariables.java; Stores variables used throughout the app - Updated build.gradle; Added multiDex support

---
+++ app/src/main/java/com/ctl/votemate/GlobalVariables.java
@@ -0,0 +1,9 @@
+package com.ctl.votemate;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class GlobalVariables {
+    public static FirebaseAuth globalAuth;
+    public static FirebaseUser globalUser;
+}
