ba767840dd47582b95fd5c13cf2db9002a228463
Chris <chrismc16@outlook.com>
2023-03-11 21:09:15 -0800

    - Updated PageLogin.java; Added "displayErrorDialog" function to display a simple dialog with a custom message, & "isValidEmail" function to check if a string is considered a valid email address

--- app/src/main/java/com/ctl/votemate/PageLogin.java
+++ app/src/main/java/com/ctl/votemate/PageLogin.java
@@ -1,9 +1,15 @@
 package com.ctl.votemate;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PageLogin extends AppCompatActivity {
 
@@ -16,5 +22,23 @@ public class PageLogin extends AppCompatActivity {
         buttonBack.setOnClickListener(n -> {
             finish();
         });
+    }
+
+    private void displayErrorDialog(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(msg).setNeutralButton("Ok", (dialog, which) -> {
+            dialog.dismiss();
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public boolean isValidEmail(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+
+        return ((Matcher) matcher).matches();
     }
 }
