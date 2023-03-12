07d77774cbec4ba49c700bd675893e9f4b8eb3c1
Chris <chrismc16@outlook.com>
2023-03-11 21:11:42 -0800

    - Updated MainActivity.java; Added support for Firebase authentication - Updated activity_main.xml; "Login" & "Register" buttons now have a ripple effect when being clicked

--- app/src/main/java/com/ctl/votemate/MainActivity.java
+++ app/src/main/java/com/ctl/votemate/MainActivity.java
@@ -6,25 +6,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GlobalVariables.globalAuth = FirebaseAuth.getInstance();
+        GlobalVariables.globalUser = GlobalVariables.globalAuth.getCurrentUser();
 
-        //Declare UI Elements
-        LinearLayout buttonLoginPage = findViewById(R.id.buttonLogin);
-        LinearLayout buttonRegisterPage = findViewById(R.id.buttonRegister);
-
-        //Navigation Buttons
-        buttonLoginPage.setOnClickListener(v -> {
+        if (GlobalVariables.globalUser != null) {
             Intent pageLogin = new Intent(this,PageLogin.class);
             startActivity(pageLogin);
-        });
-        buttonRegisterPage.setOnClickListener(v ->{
-            Intent pageRegister = new Intent(this, PageRegister.class);
-            startActivity(pageRegister);
-        });
+        } else {
+            //Declare UI Elements
+            MaterialCardView buttonLoginPage = findViewById(R.id.buttonLogin);
+            MaterialCardView buttonRegisterPage = findViewById(R.id.buttonRegister);
+
+            //Navigation Buttons
+            buttonLoginPage.setOnClickListener(v -> {
+                Intent pageLogin = new Intent(this,PageLogin.class);
+                startActivity(pageLogin);
+            });
+            buttonRegisterPage.setOnClickListener(v ->{
+                Intent pageRegister = new Intent(this, PageRegister.class);
+                startActivity(pageRegister);
+            });
+        }
     }
 }
